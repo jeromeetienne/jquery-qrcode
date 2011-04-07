6cf004f07e4d93b8ac48b20e217ffe2ef3621874
Jerome Etienne <jerome.etienne@gmail.com>
2011-04-07 08:41:55 +0200

    cleanup + folder refactoring

---
+++ README.md
@@ -0,0 +1,8 @@
+# jQuery.qrcode.js
+
+with jQuery.qrcode.js you can easily add qrcode to your webpages.
+
+    jQuery("#container").qrcode("this plugin is great")
+
+It is standalone, no external services which go on and off, or add latency
+while loading.
---
+++ src/jquery.qrcode.js
@@ -0,0 +1,47 @@
+(function( $ ){
+	$.fn.qrcode = function(options) {
+
+		// if options is string, 
+		if( typeof options === 'string' ){
+			options	= { text: options };
+		}
+
+		// set default values
+		options	= $.extend( {}, {
+			width		: 256,
+			height		: 256,
+			typeNumber	: 4,
+			correctLevel	: QRErrorCorrectLevel.H
+		}, options);
+
+		var createCanvas	= function(){
+			// create the qrcode itself
+			var qrcode	= new QRCode(options.typeNumber, options.correctLevel);
+			qrcode.addData(options.text);
+			qrcode.make();
+
+			// create canvas element
+			var canvas	= document.createElement('canvas');
+			canvas.width	= options.width;
+			canvas.height	= options.height;
+			var ctx		= canvas.getContext('2d');
+			
+			var tileW	= options.width / qrcode.getModuleCount();
+			var tileH	= options.height / qrcode.getModuleCount();
+	
+			for( var row = 0; row < qrcode.getModuleCount(); row++ ){
+				for( var col = 0; col < qrcode.getModuleCount(); col++ ){
+					ctx.fillStyle = qrcode.isDark(row, col) ? "#000000" : "#ffffff";
+					ctx.fillRect( col*tileW, row*tileH, tileW, tileH );  
+				}	
+			}
+			// return just built canvas
+			return canvas;
+		}
+
+		return this.each(function(){
+			var canvas	= createCanvas();
+			jQuery(canvas).appendTo(this);
+		});
+	};
+})( jQuery );
