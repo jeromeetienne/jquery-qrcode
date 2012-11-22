6a2500e818c56281b2cc2b659fdfa479d9c03f34
TimZhang <timzhang@dnspod.com>
2012-11-22 20:33:36 +0800

    ensure the width and height of the table are equal when render with 'table' suppliment

--- src/jquery.qrcode.js
+++ src/jquery.qrcode.js
@@ -55,8 +55,6 @@
 			
 			// create table element
 			var $table	= $('<table></table>')
-				.css("width", options.width+"px")
-				.css("height", options.height+"px")
 				.css("border", "0px")
 				.css("border-collapse", "collapse")
 				.css('background-color', options.background)
