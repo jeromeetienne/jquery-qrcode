93c7ad821dc5b4fa5f2bdc85065c0894806f6b20
ginlime <webmaster@emeraldshell.com>
2018-04-05 09:32:58 +0900

    Added some goodies
    * added "renderend" event
    * added "png" for "render" option
    * added "padding" option according specification of QR Code.

--- src/jquery.qrcode.js
+++ src/jquery.qrcode.js
@@ -1,5 +1,7 @@
 (function( $ ){
 	$.fn.qrcode = function(options) {
+		// base number for auto padding
+		var paddingBase = 4;
 		// if options is string, 
 		if( typeof options === 'string' ){
 			options	= { text: options };
@@ -11,6 +13,7 @@
 			render		: "canvas",	// canvas, table, png
 			width		: 256,
 			height		: 256,
+			padding		: "auto",	// numeric or auto ("auto" adds 4 cells for padding around each)
 			typeNumber	: -1,
 			correctLevel	: QRErrorCorrectLevel.H,
                         background      : "#ffffff",
@@ -30,16 +33,23 @@
 			var ctx		= canvas.getContext('2d');
 
 			// compute tileW/tileH based on options.width/options.height
-			var tileW	= options.width  / qrcode.getModuleCount();
-			var tileH	= options.height / qrcode.getModuleCount();
+			var moduleCount = qrcode.getModuleCount();
+			var tileW	= (options.padding == "auto") ? options.width / (moduleCount+paddingBase*2) : (options.width - options.padding*2)  / moduleCount;
+			var tileH	= (options.padding == "auto") ? options.height / (moduleCount+paddingBase*2) : (options.height - options.padding*2) / moduleCount;
 
 			// draw in the canvas
-			for( var row = 0; row < qrcode.getModuleCount(); row++ ){
-				for( var col = 0; col < qrcode.getModuleCount(); col++ ){
+			// filling base
+			ctx.fillStyle = options.background;
+			ctx.fillRect(0,0,options.width,options.height);
+			// start position
+			var startLeft = (options.padding == "auto") ? Math.round(tileW*paddingBase) : options.padding;
+			var startTop = (options.padding == "auto") ? Math.round(tileH*paddingBase) : options.padding;
+			for( var row = 0; row < moduleCount; row++ ){
+				for( var col = 0; col < moduleCount; col++ ){
 					ctx.fillStyle = qrcode.isDark(row, col) ? options.foreground : options.background;
 					var w = (Math.ceil((col+1)*tileW) - Math.floor(col*tileW));
 					var h = (Math.ceil((row+1)*tileW) - Math.floor(row*tileW));
-					ctx.fillRect(Math.round(col*tileW),Math.round(row*tileH), w, h);  
+					ctx.fillRect(Math.round(col*tileW)+startLeft,Math.round(row*tileH)+startTop, w, h);
 				}	
 			}
 			// return just built canvas
@@ -62,19 +72,29 @@
 				.css('background-color', options.background);
 		  
 			// compute tileS percentage
-			var tileW	= options.width / qrcode.getModuleCount();
-			var tileH	= options.height / qrcode.getModuleCount();
+			var moduleCount = qrcode.getModuleCount();
+			var tileW	= (options.padding == "auto") ? options.width / (moduleCount+paddingBase*2) : (options.width - options.padding*2)  / moduleCount;
+			var tileH	= (options.padding == "auto") ? options.height / (moduleCount+paddingBase*2) : (options.height - options.padding*2) / moduleCount;
 
 			// draw in the table
-			for(var row = 0; row < qrcode.getModuleCount(); row++ ){
+			for(var row = 0; row < moduleCount; row++ ){
 				var $row = $('<tr></tr>').css('height', tileH+"px").appendTo($table);
 				
-				for(var col = 0; col < qrcode.getModuleCount(); col++ ){
+				for(var col = 0; col < moduleCount; col++ ){
 					$('<td></td>')
 						.css('width', tileW+"px")
 						.css('background-color', qrcode.isDark(row, col) ? options.foreground : options.background)
 						.appendTo($row);
-				}	
+				}
+				if(options.padding){
+					var paddingWidth = (options.padding == "auto") ? tileW*paddingBase : options.padding;
+					var paddingCell = $('<td></td>').css('width', paddingWidth+"px").css('background-color',options.background);
+					$row.prepend(paddingCell).append(paddingCell);
+				}
+			}
+			if(options.padding){
+				var paddingRow = $('<tr></tr>').css('height', tileH+"px").append($('<td></td>').attr('colspan',moduleCount+2).css('background-color',options.background));
+				$table.prepend(paddingRow).append(paddingRow);
 			}
 			// return just built canvas
 			return $table;
@@ -82,13 +102,14 @@
   
 
 		return this.each(function(){
-			var element	= (options.render == "canvas" || options.render == "png") == "canvas" ? createCanvas() : createTable();
+			var element	= (options.render == "canvas" || options.render == "png") ? createCanvas() : createTable();
 			if(options.render == "png"){
 				var png = element.toDataURL();
 				$('<img>').attr('src',png).appendTo(this);
 			} else {
 				$(element).appendTo(this);
 			}
+			$(this).trigger('renderend');
 		});
 	};
 })( jQuery );
